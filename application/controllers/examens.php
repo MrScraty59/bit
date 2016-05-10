3d3376c1151081e68e7bc857d78712a3286dc022
T4GAD4 <T4GAD4@DESKTOP-8M4FHAB>
2016-05-10 15:26:21 +0200

    Modify controller examen and model exam

--- application/controllers/examens.php
+++ application/controllers/examens.php
@@ -8,6 +8,26 @@ class Examens extends CI_Controller {
             if($idMatiere == 0){
                 redirect('/accueil');
             }
-            echo 'test';
+            
+            $this->form_validation->set_rules('cours','Cours','trim|xss_clean|php_encode_tags|required|number');
+            $this->form_validation->set_rules('','','');
+            $this->form_validation->set_rules('','','');
+            $this->form_validation->set_rules('','','');
+            $this->form_validation->set_rules('','','');
+            $this->form_validation->set_rules('','','');
+            
+            if($this->input->post() && $this->form_validation->run()){
+                
+                $examen = new stdClass();
+                $examen->idCours = $this->input->post('cours');
+                $examen->debutExam = $this->input->post('debutExam');
+                $examen->finExam = $this->input->post('finExam');
+                $examen->nom = $this->input->post('nom');
+                $examen->id = $this->examen->add($examen);                
+            }
+            
+            $this->load->view('template/header');
+            $this->load->view('pages/examens/creer');
+            $this->load->view('template/footer');
 	}
 }
