4c790c239d6ce8395c28f7d9af931357989ac119
MrScraty59 <matthieu.leclerc@epsi.fr>
2016-05-10 15:59:31 +0200

    liste examens

---
+++ application/views/pages/examens/liste.php
@@ -0,0 +1,35 @@
+
+<div class="row">
+    <div class="col-sm-offset-2 col-sm-8">
+        <div class="row">
+            <div class="col-sm-offset-10 col-sm-2">
+                <a href="<?php echo base_url();?>/examens/creer" class="btn btn-default">New examen</a>
+            </div>
+        </div>
+        <table width="100%">
+            <thead>
+                <tr>
+                    <th width="80%">Titre</th>
+                    <th width="20%" style="text-align:center;">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Examen1</td>
+                    <td style="text-align:center;"><i class="fa fa-cog"></i> <i class="fa fa-edit"></i> <i class="fa fa-times"></i></td>
+                </tr>
+                <tr>
+                    <td>Examen2</td>
+                    <td style="text-align:center;"><i class="fa fa-cog"></i> <i class="fa fa-edit"></i> <i class="fa fa-times"></i></td>
+                </tr>
+                <tr>
+                    <td>Examen3</td>
+                    <td style="text-align:center;"><i class="fa fa-cog"></i> <i class="fa fa-edit"></i> <i class="fa fa-times"></i></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<script>
+    
+</script>
