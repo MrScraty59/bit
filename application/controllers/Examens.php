2f9b6be0e6f6ca3d8962ffcc40f42f928df33310
T4GAD4 <T4GAD4@DESKTOP-8M4FHAB>
2016-05-12 10:04:06 +0200

    Controlleurs

--- application/controllers/Examens.php
+++ application/controllers/Examens.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Examens extends CI_Controller {
+class Examens extends CI_Controller { 
 
     public function index() {
         $data = Array();
